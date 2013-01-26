3a203eb1d23400a8cdf6063372e213fd84088428
kevin <xiaohuidexinge@163.com>
2013-01-26 18:32:17 +0800

    添加一个 去重复字的 工具  用来抓字库

---
+++ tools/zhuazi/zhuazi.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+# for generate XPanel children
+
+use strict;
+use warnings;
+
+use Encode;
+use Encode::CN;
+my %hash = ();
+
+my @all_lines = ();
+
+while(<>){
+    chomp;
+    push @all_lines, $_;
+}
+
+for my $line (@all_lines){
+    Encode::_utf8_on($line); 
+    for( my $index = 0; $index < length $line; $index++ ){
+        my $one = substr $line, $index, 1;
+        $hash{$one}++;
+    }
+}
+
+print keys %hash;
+print "\n";
+print STDERR scalar( keys %hash );
+print "\n";
