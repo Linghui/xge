1f07f2b78ec2dd1218a08aed4e3ddf26322574f3
kevin <xiaohuidexinge@163.com>
2013-03-20 00:54:27 +0800

    AvoidOnScreenKeyboard, 用来阻止 屏幕键盘弹出。 XGETextInputListener 用来处理编辑框文字输入。

---
+++ src/com/gol/xge/rpg/ui/XGETextInputListener.java
@@ -0,0 +1,36 @@
+package com.gol.xge.rpg.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.TextInputListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+public class XGETextInputListener extends InputListener {
+
+    private TextField textField;
+    private String title;
+    
+    public XGETextInputListener(TextField textField, String title){
+        this.textField = textField;
+        this.title = title;
+    }
+    
+    public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+        Gdx.input.getTextInput(new TextInputListener(){
+
+            @Override
+            public void input(String text) {
+                textField.setText(text);
+            }
+
+            @Override
+            public void canceled() {
+                // TODO Auto-generated method stub
+                
+            }
+            
+        }, title, "");
+        return true;
+    }
+}
