ba62618b9f3bd1002f15662b93af83d5b79c6335
kevin <xiaohuidexinge@163.com>
2013-04-21 19:21:50 +0800

    添加一个新控件， 用来显示 动态变化增长数字。 有需要以后可以添加 减少数字。

---
+++ src/com/gol/xge/rpg/ui/CountingLabel.java
@@ -0,0 +1,78 @@
+package com.gol.xge.rpg.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+
+public class CountingLabel extends Group {
+
+    private int totalNum = 0;
+    private int showNum = 0;    // the number showing jumping
+    private int numAddOn = 0;   // number added on totalNum
+    
+    private float totalMicSec = 0;
+    private float actingTime = 0f;
+    private float numJumpSec = 0f;
+    private float numJumpCounter = 0f;
+    
+    private final float default_jump_sec = 0.03f;
+    
+    private Label num;
+
+    public CountingLabel(LabelStyle style){
+        num = new Label("" + totalNum, style);
+        this.setWidth(100);
+        this.setHeight(num.getHeight());
+        this.addActor(num);
+    }
+    
+    public void act (float delta) {
+        super.act(delta);
+        if( numAddOn == 0 ){
+            return;
+        }
+        
+        actingTime += delta;
+        
+        // time passed, set number to it should be
+        if( actingTime >= totalMicSec){
+            numAddOn = 0;
+            actingTime = 0f;
+            numJumpCounter = 0f;
+            num.setText("" + (totalNum));
+            return;
+        }
+        
+        numJumpCounter += delta;
+        if( numJumpCounter < numJumpSec ){
+            return;
+        }
+        
+        // animation number showing
+        float per = (float)numJumpCounter/ totalMicSec;
+        Gdx.app.log("111", "perl " + per);
+        int temp = (int) (per * numAddOn);
+        Gdx.app.log("111", "temp " + temp);
+        num.setText("" + (showNum+=temp));
+        numJumpCounter = 0f;
+    }
+    
+    public void addOnNum( int num ){
+        this.addOnNum(num, 1f);
+    }
+    
+    public void addOnNum( int num, float seconds ){
+        if( seconds < 1){
+            seconds = 1;
+        }
+        // record this to colulate how much time passed, so how many number add on on every act(delta)
+        totalMicSec = seconds;
+        float temp = seconds / 30;
+        numJumpSec =  temp > default_jump_sec?default_jump_sec:temp;
+        
+        numAddOn = num;
+        showNum = totalNum;
+        totalNum += numAddOn;
+    }
+}
