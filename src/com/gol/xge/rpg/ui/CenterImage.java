7ef10bc49a6f3ebe7c6376c6ce6093f7df217f56
kevin <xiaohuidexinge@163.com>
2013-05-29 16:56:17 +0800

    添加drawable 构造

--- src/com/gol/xge/rpg/ui/CenterImage.java
+++ src/com/gol/xge/rpg/ui/CenterImage.java
@@ -3,6 +3,7 @@ package com.gol.xge.rpg.ui;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class CenterImage extends Group {
 
@@ -13,6 +14,14 @@ public class CenterImage extends Group {
         this.addActor(image);
     }
 
+    
+    public CenterImage( Drawable drawable){
+        Image image = new Image(drawable);
+        image.setX(-image.getWidth()/2);
+        image.setY(-image.getHeight()/2);
+        this.addActor(image);
+    }
+    
     public void setFlipX(boolean flipX) {
         
     }
