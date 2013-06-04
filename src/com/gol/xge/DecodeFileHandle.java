3efd2df3a69b2c8bc914c0a09bf3c210ae1c0926
kevin <xiaohuidexinge@163.com>
2013-06-04 10:17:27 +0800

    让 decodeFileHandler 覆盖更多方法。

--- src/com/gol/xge/DecodeFileHandle.java
+++ src/com/gol/xge/DecodeFileHandle.java
@@ -1,8 +1,11 @@
 package com.gol.xge;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.Gdx;
@@ -12,8 +15,15 @@ public class DecodeFileHandle extends FileHandle {
 
     private FileHandle fh;
     private String code = "test";
+    static String addtionalKey = "test";//密码
+    
+
+//    private int code = 0;
+    
     public DecodeFileHandle(FileHandle fh, String code) {
         super(fh.path(), fh.type());
+        Gdx.app.log("DecodeFileHandle", "fh.path() " + fh.path());
+        Gdx.app.log("DecodeFileHandle", "fh.type() " + fh.type());
         this.fh = fh;
         this.code = code;
     }
@@ -26,10 +36,12 @@ public class DecodeFileHandle extends FileHandle {
     public byte[] readBytes () {
         Gdx.app.log("DecodeFileHandle", "readBytes");
         if( code == null ){
-            return super.readBytes();
+            return fh.readBytes();
         }
+        code += addtionalKey;
+        
         int value = -1;
-        InputStream is = super.read();
+        InputStream is = fh.read();
         int cx = 0;
         byte[] decodeBytes = new byte[(int) super.length()];
         Gdx.app.log("DecodeFileHandle", " super.length() " + super.length() + " " + fh.name());
@@ -45,9 +57,38 @@ public class DecodeFileHandle extends FileHandle {
         
         return decodeBytes;
     }
+    public InputStream read () {
+        return fh.read();
+    }
+    
+    public BufferedInputStream read (int bufferSize) {
+        return fh.read(bufferSize);
+    }
+    
+    public Reader reader () {
+        return fh.reader();
+    }
 
+    public Reader reader (String charset) {
+        return fh.reader(charset);
+    }
+    
+    public BufferedReader reader (int bufferSize) {
+        return fh.reader(bufferSize);
+    }
+    
+    public BufferedReader reader (int bufferSize, String charset) {
+        return fh.reader(bufferSize, charset);
+    }
+    
+    public String readString () {
+        return fh.readString(null);
+    }
+    public String readString (String charset) {
+        return fh.readString(charset);
+    }
+    
     @Override
-
     public FileHandle parent () {
         Gdx.app.log("DecodeFileHandle ", "parent ");
         File parent = file.getParentFile();
@@ -65,6 +106,13 @@ public class DecodeFileHandle extends FileHandle {
         Gdx.app.log("DecodeFileHandle ", "child ");
         if (file.getPath().length() == 0) return new DecodeFileHandle(new File(name), fh.type());
         return new DecodeFileHandle(new File(file, name), type);
+    }
+    
+    public boolean exists () {
+        if( fh == null ){
+            return false;
+        }
+        return fh.exists();
     }
     
 }
