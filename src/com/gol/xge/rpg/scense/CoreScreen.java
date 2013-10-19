834e43c0f9cdc27dabfd5bdb44216999caa2ba18
kevin <xiaohuidexinge@163.com>
2013-10-19 12:29:33 +0800

    修改 纹理渲染，牺牲效率要质量。

--- src/com/gol/xge/rpg/scense/CoreScreen.java
+++ src/com/gol/xge/rpg/scense/CoreScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -297,6 +298,15 @@ public abstract class CoreScreen implements Screen, InputProcessor {
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         Gdx.gl.glEnable(GL10.GL_BLEND);
         Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl20.glTexParameterf(GL20.GL_TEXTURE_2D,
+                 GL20.GL_TEXTURE_MAG_FILTER,
+                 GL20.GL_LINEAR);
+        
+        Gdx.gl20.glTexParameterf(GL20.GL_TEXTURE_2D,
+                 GL20.GL_TEXTURE_MIN_FILTER,
+                 GL20.GL_LINEAR);
+
+
         backgroundStage.act(delta);
         rootStage.act(delta);
         backgroundStage.draw();
