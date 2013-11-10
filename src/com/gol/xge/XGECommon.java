6872c20f8b10a1dbd37bd379489bc6864655226f
kevin <xiaohuidexinge@163.com>
2013-11-10 22:59:50 +0800

    慢慢合并 common，有俩common  有点儿2

--- src/com/gol/xge/XGECommon.java
+++ src/com/gol/xge/XGECommon.java
@@ -1,8 +1,19 @@
 package com.gol.xge;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.JsonValue;
+import com.gol.xge.rpg.ui.AnimationGroup;
+import com.gol.xge.rpg.ui.AnimationResource;
 
 public class XGECommon {
 
@@ -55,4 +66,73 @@ public class XGECommon {
         
     }
     
+    public static AnimationGroup readAnimationGroup(
+            JsonValue json,
+            TextureAtlas atlas){
+        AnimationResource ar = new AnimationResource(json);
+        return readAnimationGroup(ar, atlas, 0.1f);
+    }
+    
+    public static AnimationGroup readAnimationGroup(
+            AnimationResource ar,
+            TextureAtlas atlas,
+            float speed){
+
+        HashMap<String, Animation> animationHash = new HashMap<String, Animation>();
+        
+        JsonValue actionsObj = ar.getResourceMap();
+//        Gdx.app.log(TAG, "content -- " + content );
+        
+        String defaultActionName = "";
+        
+//        TextureAtlas atlas = getTextureAtlasByFile(packFile);
+        for(JsonValue action = actionsObj.child(); action != null; action = action.next()){
+            String actionName = action.name();
+            
+            if ( actionName.equals("offsetX")
+                    || actionName.equals("offsetY")) {
+                continue;
+            }
+            
+            
+//          Gdx.app.log(TAG, "actionName -====== " + actionName);
+            if( "".equals(defaultActionName) ){
+                defaultActionName = actionName;
+            }
+            
+            List<TextureRegion> actionAnimation = new ArrayList<TextureRegion>();
+            
+            for(JsonValue texIds = action.child(); texIds != null; texIds = texIds.next()){
+
+                String id = texIds.asString();
+//                Gdx.app.log(TAG, "getting ||||||||||| 'id'" + id);
+                TextureRegion one = atlas.createSprite(id);
+                actionAnimation.add(one);
+            }
+            TextureRegion[] regions = new TextureRegion[actionAnimation.size()];
+            for(int index = 0 ; index < actionAnimation.size(); index++){
+                regions[index] = actionAnimation.get(index);
+            }
+            Animation animation = new Animation(speed, regions);
+            animationHash.put(actionName, animation);
+//            Gdx.app.log(TAG, "done actionName ||||||||||| ''" + actionName);
+        }
+        
+        AnimationGroup group = new AnimationGroup(
+                animationHash
+                , ar.getOffsetX()
+                , ar.getOffsetY());
+        return group;
+    }
+    
+    // tag reminds you something
+    public static void timeStamp(String tag){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        System.out.println("time stamp - "
+        + calendar.get(Calendar.MINUTE) + ":"
+        + calendar.get(Calendar.SECOND) + ":"
+        + calendar.get(Calendar.MILLISECOND ) 
+        + " -- " + tag );
+    }
 }
--- src/com/gol/xge/rpg/ui/AnimationActor.java
+++ src/com/gol/xge/rpg/ui/AnimationActor.java
@@ -11,38 +11,39 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 
-public class AnimationActor extends Group implements AnimationActionInterface{
+public class AnimationActor extends Group implements AnimationActionInterface {
 
     private String TAG = "AnimationActor";
     protected Rectangle rectangle = null;
-    
+
     private List<String> actionAdded = new ArrayList<String>();
-    
+
     AnimationGroup animationGroup;
     TextureRegion frame;
-    
+
     private Group bottomLayer = new Group();
-    
+
     private boolean flipX = false;
-    
-    public AnimationActor(){
-        
+
+    public AnimationActor() {
+
     }
-    
-    public AnimationActor(AnimationGroup animationGroup){
+
+    public AnimationActor(AnimationGroup animationGroup) {
         this(null, animationGroup, false);
     }
-    
-    public AnimationActor(String name, AnimationGroup animationGroup, boolean flipX){
+
+    public AnimationActor(String name, AnimationGroup animationGroup,
+            boolean flipX) {
         this.setName(name);
         this.animationGroup = animationGroup;
         this.flipX = flipX;
-        
+
         this.setAnimationGroup(animationGroup);
-//        this.addActor(bottomLayer);
+        // this.addActor(bottomLayer);
     }
-    
-    public void addBottomActor( Actor actor ){
+
+    public void addBottomActor(Actor actor) {
         bottomLayer.addActor(actor);
     }
 
@@ -50,91 +51,92 @@ public class AnimationActor extends Group implements AnimationActionInterface{
      * different from setAction
      */
     @Override
-    public AnimationActor addAction(String actionName){
-        
-        
-        if( animationGroup!= null 
-                && animationGroup.hasAction(actionName)){
+    public AnimationActor addAction(String actionName) {
+
+        if (animationGroup != null && animationGroup.hasAction(actionName)) {
             actionAdded.add(actionName);
         }
         return this;
     }
-    
+
     @Override
     public void act(float delta) {
         super.act(delta);
         bottomLayer.act(delta);
-        if( animationGroup == null ){
+        if (animationGroup == null) {
             return;
         }
-        
+
         frame = animationGroup.getKeyFrame(delta);
-        
-        if( flipX ){
-            if( ! frame.isFlipX() ){
+
+        if (flipX) {
+            if (!frame.isFlipX()) {
                 frame.flip(true, false);
             }
         } else {
-            if( frame.isFlipX() ){
+            if (frame.isFlipX()) {
                 frame.flip(true, false);
             }
         }
-        
+
     }
-    
+
     @Override
     public void draw(SpriteBatch batch, float parentAlpha) {
-        if( animationGroup == null ){
+        if (animationGroup == null) {
             return;
         }
-        if( !this.isVisible() ){
+        if (!this.isVisible()) {
             return;
         }
-        
-        batch.setColor(this.getColor().r, this.getColor().g, this.getColor().b, this.getColor().a);
-        
+
+        batch.setColor(this.getColor().r, this.getColor().g, this.getColor().b,
+                this.getColor().a);
+
         bottomLayer.setX(this.getX());
         bottomLayer.setY(this.getY());
-        
+
         bottomLayer.draw(batch, parentAlpha);
-        batch.setColor(this.getColor().r, this.getColor().g, this.getColor().b, this.getColor().a);
-        
+        batch.setColor(this.getColor().r, this.getColor().g, this.getColor().b,
+                this.getColor().a);
+
         float x = this.getX() - animationGroup.getOffsetX();
-        if( frame.isFlipX() ){
+        if (frame.isFlipX()) {
             x = this.getX() - (this.getWidth() - animationGroup.getOffsetX());
         }
-        batch.draw(frame, x, this.getY() -  animationGroup.getOffsetY(), 
-                this.getOriginX(), this.getOriginY()
-                , this.getWidth(), this.getHeight()
-                , this.getScaleX(), this.getScaleY(), this.getRotation());
+        
+        batch.draw(frame, x, this.getY() - animationGroup.getOffsetY(),
+                this.getOriginX(), this.getOriginY(), this.getWidth(),
+                this.getHeight(), this.getScaleX(), this.getScaleY(),
+                this.getRotation());
         super.draw(batch, parentAlpha);
     }
-    
+
     @Override
-    public Actor hit (float x, float y, boolean touchable) {
-        return x >= -this.getWidth()/2 && x <= this.getWidth()/2 && y >= -this.getHeight()/2 && y <= this.getHeight()/2 ? this : null;
+    public Actor hit(float x, float y, boolean touchable) {
+        return x >= -this.getWidth() / 2 && x <= this.getWidth() / 2
+                && y >= -this.getHeight() / 2 && y <= this.getHeight() / 2 ? this
+                : null;
     }
-    
-    
-    public boolean getFlipX(){
+
+    public boolean getFlipX() {
         return this.flipX;
     }
-    
-    public void setFlipX( boolean flipX ){
+
+    public void setFlipX(boolean flipX) {
         this.flipX = flipX;
     }
-    
-    public void setAction(String actionName){
+
+    public void setAction(String actionName) {
         this.setAction(actionName, false);
     }
-    
 
-    public void setAction(String actionName, boolean loop){
-        if( animationGroup == null ){
+    public void setAction(String actionName, boolean loop) {
+        if (animationGroup == null) {
             return;
         }
         int playMode = 0;
-        if( loop ){
+        if (loop) {
             playMode = Animation.LOOP;
         } else {
             playMode = Animation.NORMAL;
@@ -142,40 +144,39 @@ public class AnimationActor extends Group implements AnimationActionInterface{
         this.setAction(actionName, playMode);
     }
 
-    
-    public void setAction(String actionName, int  playMode){
-        this.setAction(actionName, AnimationGroup.defaultSpeed , playMode);
+    public void setAction(String actionName, int playMode) {
+        this.setAction(actionName, AnimationGroup.defaultSpeed, playMode);
     }
-    
 
-    public void setAction(String actionName, float speed, int  playMode){
+    public void setAction(String actionName, float speed, int playMode) {
         this.animationGroup.setAction(actionName, speed, playMode);
     }
-    
-    public void setAnimationGroup(AnimationGroup animationGroup){
+
+    public void setAnimationGroup(AnimationGroup animationGroup) {
         this.animationGroup = animationGroup;
 
-        if( animationGroup == null ){
+        if (animationGroup == null) {
             return;
         }
-        
+
         frame = animationGroup.getKeyFrame(0f);
         this.setWidth(Math.abs(animationGroup.getKeyFrame(0).getRegionWidth()));
-        this.setHeight(Math.abs(animationGroup.getKeyFrame(0).getRegionHeight()));
-        this.setOrigin(this.getWidth()/2, this.getHeight()/2);
+        this.setHeight(Math
+                .abs(animationGroup.getKeyFrame(0).getRegionHeight()));
+        this.setOrigin(this.getWidth() / 2, this.getHeight() / 2);
         this.setTouchable(Touchable.enabled);
-        
+
     }
-    
-    public AnimationGroup getAnimationGroup(){
+
+    public AnimationGroup getAnimationGroup() {
         return animationGroup;
     }
 
-    public void setCollistionArea(Rectangle rectangle){
+    public void setCollistionArea(Rectangle rectangle) {
         this.rectangle = rectangle;
     }
 
-    public Rectangle getCollistionArea(){
+    public Rectangle getCollistionArea() {
         return rectangle;
     }
 
